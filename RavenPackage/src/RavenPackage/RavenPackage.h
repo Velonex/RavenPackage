59b47d650473f4f3d78598d2a421ec988664bcd4
Jonathan Steitzer <joni.steitzer@gmail.com>
2020-06-29 22:27:55 +0200

    Added logging define guards for custom output possibilities

--- RavenPackage/src/RavenPackage/RavenPackage.h
+++ RavenPackage/src/RavenPackage/RavenPackage.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+// Logging defines for possibility of custom logging
+#ifndef RPK_TRACE
+#define RPK_TRACE(str) std::cout << "RavenPackage-Trace: " << str << std::endl;
+#endif
+#ifndef RPK_ERROR
+#define RPK_ERROR(str) std::cerr << "RavenPackage-Error: " << str << std::endl;
+#endif
+
 namespace rvn {
 	namespace package {
+
 
 	}
 }
